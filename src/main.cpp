72c4aedc518fe9f54fea5216c3e1ebd1610fe5a8
Virgil <mrchan3668@gmail.com>
2020-01-28 22:32:46 +0800

    feat: 🎸 add third library

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
+#include <boost/algorithm/string/replace.hpp>
+#include <string>
 #include "utils/hash.h"
 
 using namespace std;
+using namespace boost::algorithm;
 
 int main()
 {
-  double num1, num2, result = 0;
-  char sign[] = "";
-  cout << "Input First Number" << endl;
-  cin >> num1;
-  cout << "Input Sign" << endl;
-  cin >> sign;
-  cout << "Input Second Number" << endl;
-  cin >> num2;
-  switch (hash_(sign))
-  {
-  case "+"_hash:
-    result = num1 + num2;
-    break;
-  case "-"_hash:
-    result = num1 - num2;
-    break;
-  case "*"_hash:
-    result = num1 * num2;
-    break;
-  case "/"_hash:
-    result = num1 / num2;
-    break;
-  case "%"_hash:
-    result = int(num1) % int(num2);
-    break;
-  default:
-    break;
-  }
+  int result = 0;
+  string expr = "";
+  cout << "Input Calc Function" << endl;
+  cin >> expr;
+  replace_all(expr, " ", "");
+  cout << "expr: " << expr << endl;
+
+  // switch (hash_(sign))
+  // {
+  // case "+"_hash:
+  //   result = num1 + num2;
+  //   break;
+  // case "-"_hash:
+  //   result = num1 - num2;
+  //   break;
+  // case "*"_hash:
+  //   result = num1 * num2;
+  //   break;
+  // case "/"_hash:
+  //   result = num1 / num2;
+  //   break;
+  // case "%"_hash:
+  //   result = int(num1) % int(num2);
+  //   break;
+  // default:
+  //   break;
+  // }
   cout << "result is: " << result << endl;
 }
