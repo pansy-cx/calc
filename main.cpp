761c93903a1c141bb47a976f629bf9e42fe52c4d
Virgil <mrchan3668@gmail.com>
2020-01-28 10:18:26 +0800

    fix: 🐛 string -> char

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main()
 {
   double num1, num2, result = 0;
-  string sign = "";
+  char sign[] = "";
   cout << "Input First Number" << endl;
   cin >> num1;
   cout << "Input Sign" << endl;
   cin >> sign;
   cout << "Input Second Number" << endl;
   cin >> num2;
-  if (sign == "+") {
+  if (strcmp(sign, "+") == 0) {
     result = num1 + num2;
-  } else if (sign == "-") {
+  } else if (strcmp(sign, "-") == 0) {
     result = num1 - num2;
-  } else if (sign == "*") {
+  } else if (strcmp(sign, "*") == 0) {
     result = num1 * num2;
-  } else if (sign == "/") {
+  } else if (strcmp(sign, "/") == 0) {
     result = num1 / num2;
-  } else if (sign == "%") {
+  } else if (strcmp(sign, "%") == 0) {
     result = int(num1) % int(num2);
   }
   cout << "result is: " << result << endl;
